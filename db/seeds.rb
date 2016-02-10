d893619e46886430487b16e3281884c2059448d8
Andrew Harrison <a.git14@uio9.org>
2016-02-10 10:32:31 +0100

    Seed Artists.
    To execute the seeds.rb file
    
    > rake db:seed
    
    or
    
    > rake db:reset
    
    The reset command completely additionally resets the database, and all new ID counters begin again at 1.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Artist
+
+Artist.destroy_all
+
+daft_punk        = Artist.create name: "Daft Punk"       , country: "FR"
+pink_floyd       = Artist.create name: "Pink Floyd"      , country: "GB"
+armin_van_buuren = Artist.create name: "Armin van Buuren", country: "NL"
+david_bowie      = Artist.create name: "David Bowie"     , country: "GB"
+snoop_dogg       = Artist.create name: "Snoop Dogg"      , country: "US"
